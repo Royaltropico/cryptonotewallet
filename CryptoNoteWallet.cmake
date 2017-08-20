98eeb555d055b27579c3fed9572c05ad6cbb5e31
Tropico Hedge <support@tropicohedge.org>
2017-08-20 05:30:58 +0200

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "tropicohedge")
+set(CN_CURRENCY_DISPLAY_NAME "Tropico Hedge")
+set(CN_CURRENCY_TICKER "TPO")
